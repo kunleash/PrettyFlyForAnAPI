80f5d29e95052c3ed03c95f610b0b9999896b284
Thanos <thanoskan@yahoo.gr>
2017-01-24 23:24:50 +0200

    Updated Initial Frame

---
+++ app/src/main/java/com/example/thanos/prettyflyforanapi/FetchFlights.java
@@ -0,0 +1,8 @@
+package com.example.thanos.prettyflyforanapi;
+
+/**
+ * Created by Thanos on 24/1/2017.
+ */
+
+public class FetchFlights {
+}
--- app/src/main/java/com/example/thanos/prettyflyforanapi/MainActivity.java
+++ app/src/main/java/com/example/thanos/prettyflyforanapi/MainActivity.java
@@ -1,31 +1,31 @@
 package com.example.thanos.prettyflyforanapi;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_item_flight);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -49,7 +49,22 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    private void PushUp(){
-        int i = 1+1;
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+
+
+            View rootView = inflater.inflate(R.layout.list_item_flight, container, false);
+
+
+
+            return rootView;
+        }
     }
+
 }
