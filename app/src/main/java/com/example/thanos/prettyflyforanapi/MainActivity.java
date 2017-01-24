52b3a2066f6cdf89b5a215b39c3ec8ea40b1238b
it12117 <it12117@uom.edu.gr>
2017-01-25 00:04:48 +0200

    test

--- app/src/main/java/com/example/thanos/prettyflyforanapi/MainActivity.java
+++ app/src/main/java/com/example/thanos/prettyflyforanapi/MainActivity.java
@@ -57,11 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-
-
+            
             View rootView = inflater.inflate(R.layout.list_item_flight, container, false);
-
-
 
             return rootView;
         }
